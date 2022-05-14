e4a1146c9db09106c90c94cc40bad2fda6758e4a
hannahhan153 <hannah.han@biola.edu>
2022-05-13 22:20:51 -0700

    add navbar that includes about me, contact me, portfolio and resume as categories

--- react-portfolio/src/components/About/index.js
+++ react-portfolio/src/components/About/index.js
@@ -4,7 +4,7 @@ import coverImage from "../../assets/cover/coverimageforportfolio.jpeg";
 function About() {
     return (
         <section>
-            <h1 id="about">Hello, my name is Hannah Han. I had the opportunity to be in the Columbia University's Boot Camp for Full Stack Web Development. I learned many skills and languages. Through this program, I learned I am mostly interested in back-end development, and I am self-motivated and optimistic about finding a solution to a bug or issue I find. 
+            <h1 id="about">Hello, my name is Hannah Han. I had the opportunity to be in the Columbia University's Boot Camp for Full Stack Web Development from 2021 to 2022. I learned many skills and languages. Through this program, I learned I am mostly interested in back-end development, and I am self-motivated and optimistic about finding a solution to a bug or issue I find. 
             The portfolio section of this application will show the specific languages I used throughout this program and the skills I applied to optimize functionality. 
               </h1>
             <img src={coverImage} className="my-2" style={{ width: "80%" }} alt="cover" />
--- react-portfolio/src/components/Nav/index.js
+++ react-portfolio/src/components/Nav/index.js
@@ -1,8 +1,44 @@
 import React from 'react';
 
+function categorySelected(name) {
+    console.log(`${name} clicked`)
+}
+
 function Nav() {
+    const categories = [
+        {
+            name: "Portfolio",
+            description:
+              "Images of six of the developer's applications with links to both the deployed applications and the corresponding GitHub repositoryare displayed",
+          },
+          { name: "Resume", description: "A link to a downloadable resume and a list of the developer’s proficiencies are displayed"}
+    ];
     return (
-        <header></header>
+        <header>
+        <h1>
+            <a href="/">
+                <span role="img" aria-label="life"> 🌱 </span> Portfolio by Hannah Han
+            </a>
+        </h1>
+        <nav>
+        <ul className ="flex-row">
+            <li className="mx-3">
+                <a href="#about">About Me</a>
+            </li>
+            <li> 
+                <a href="contact"> Contact Me</a>
+            </li>
+            {categories.map((category) => (
+                <li className="mx-1" key={category.name}
+                >
+                    <span onClick={() => categorySelected(category.name)}>
+                    {category.name}
+                    </span>
+                    </li>
+            ))}
+        </ul>
+        </nav>
+        </header>
     );
 }
 
