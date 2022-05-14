c0ce8a7ce72dd3c626983299acd573097a36cc67
hannahhan153 <hannah.han@biola.edu>
2022-05-14 16:13:18 -0700

    add tests

--- react-portfolio/src/components/Nav/index.js
+++ react-portfolio/src/components/Nav/index.js
@@ -13,25 +13,34 @@ function Nav() {
           },
           { name: "Resume", description: "A link to a downloadable resume and a list of the developer’s proficiencies are displayed"}
     ];
+
+    const handleClick = (item) => {
+        console.log(item);
+        return item;
+      };
+
+
     return (
-        <header>
+        <header className="flex-row px-1">
         <h1>
-            <a href="/">
+            <a data-testid="link" href="/">
                 <span role="img" aria-label="life"> 🌱 </span> Portfolio by Hannah Han
             </a>
         </h1>
         <nav>
         <ul className ="flex-row">
-            <li className="mx-3">
-                <a href="#about">About Me</a>
+            <li className="mx-2">
+                <a data-testid="about" href="#about" onClick={() => handleClick("About")}>About Me</a>
             </li>
-            <li> 
+            <li className={"mx-2"}>
+                <span onClick={() => handleClick('Contact')}>
+                </span> 
                 <a href="contact"> Contact Me</a>
             </li>
             {categories.map((category) => (
                 <li className="mx-1" key={category.name}
                 >
-                    <span onClick={() => categorySelected(category.name)}>
+                    <span onClick={() => { handleClick(category.name); }}>
                     {category.name}
                     </span>
                     </li>
